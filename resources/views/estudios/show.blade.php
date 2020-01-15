275f756a3cc27ce8b66535c4977553829f7c1ab8
Manuel Fernandez <mfernandezphp@gmail.com>
2020-01-15 13:40:04 -0400

    revisando modales

--- resources/views/estudios/show.blade.php
+++ resources/views/estudios/show.blade.php
@@ -104,6 +104,9 @@
 
 @section('js')
 
+    @include('estudios.modal_descripcion')
+    @include('estudios.modal_documento')
+
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.js"></script>
@@ -144,6 +147,5 @@
 
     </script>
 
-    @include('estudios.modal_descripcion')
-    @include('estudios.modal_documento')
+
 @endsection
