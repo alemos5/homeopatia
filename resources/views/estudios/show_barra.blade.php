406965ba113a3f6e96e61e825d72b9705f478a4b
Manuel Fernandez <mfernandezphp@gmail.com>
2020-01-21 01:21:09 -0400

    Haciendo ajustes

--- resources/views/estudios/show_barra.blade.php
+++ resources/views/estudios/show_barra.blade.php
@@ -2,21 +2,21 @@
 <div class="row">
     {{--                <div class="col-sm-12">--}}
     <div style="width: 100%; height:55px;" class="progress">
-        <div class="progress-bar bg-danger" role="progressbar" style="width: <?php echo $result['reino']["animal"]; ?>%; font-size: 30px; " aria-valuenow="<?php echo $result['reino']["animal"]; ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $result['reino']["animal"]; ?>%</div>
-        <div class="progress-bar bg-success" role="progressbar" style="width: <?php echo $result['reino']["vegetal"]; ?>%; font-size: 30px; " aria-valuenow="<?php echo $result['reino']["vegetal"]; ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $result['reino']["vegetal"]; ?>%</div>
+        <div class="progress-bar" role="progressbar" style="background-color: #ff465f;width: <?php echo $result['reino']["animal"]; ?>%; font-size: 30px; " aria-valuenow="<?php echo $result['reino']["animal"]; ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $result['reino']["animal"]; ?>%</div>
+        <div class="progress-bar" role="progressbar" style="background-color: #35d238 ;width: <?php echo $result['reino']["vegetal"]; ?>%; font-size: 30px; " aria-valuenow="<?php echo $result['reino']["vegetal"]; ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $result['reino']["vegetal"]; ?>%</div>
         <div class="progress-bar bg-info" role="progressbar" style="width: <?php echo $result['reino']["mineral"]; ?>%; font-size: 30px; " aria-valuenow="<?php echo $result['reino']["mineral"]; ?>" aria-valuemin="0" aria-valuemax="100"><?php echo $result['reino']["mineral"]; ?>%</div>
     </div>
     {{--                </div>--}}
 </div>
 <div class="row">
     {{--                <div style="width: 100%" class="col-sm-12">--}}
-    <div style="width: <?php echo $result['reino']["animal"]; ?>%; color: red; font-weight: bold">
+    <div style="width: <?php echo $result['reino']["animal"]; ?>%; color: #ff465f; font-weight: bold">
         <?php echo $result['reino']["animal"]; ?>% {{ _i('Animal') }}
     </div>
-    <div style="width: <?php echo $result['reino']["vegetal"]; ?>%; color: green; font-weight: bold">
+    <div style="width: <?php echo $result['reino']["vegetal"]; ?>%; color: #35d238; font-weight: bold">
         <?php echo $result['reino']["vegetal"]; ?>% {{ _i('Vegetal') }}
     </div>
-    <div style="width: <?php echo $result['reino']["mineral"]; ?>%; color: blue; font-weight: bold">
+    <div class="text-info" style="width: <?php echo $result['reino']["mineral"]; ?>%; font-weight: bold">
         <?php echo $result['reino']["mineral"]; ?>% {{ _i('Mineral') }}
     </div>
     {{--                </div>--}}
--- resources/views/estudios/show_fields.blade.php
+++ resources/views/estudios/show_fields.blade.php
@@ -1,54 +1,69 @@
 <!-- Tipo Field -->
 <h2>{{ _i('Datos Personales') }}</h2><hr>
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('Tipo de Estudio') }}</b></div>
-    <div class="col-sm-8">{!! $estudios->tipo !!}</div>
-</div>
-@if($estudios->tipo=='humano')
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('Nombre') }}</b></div>
-    <div class="col-sm-8">{!! $estudios->h_nombre !!}</div>
-</div>
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('Apellido') }}</b></div>
-    <div class="col-sm-8">{!! $estudios->h_apellido !!}</div>
-</div>
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('Apodo') }}</b></div>
-    <div class="col-sm-8">{!! $estudios->h_identifica !!}</div>
-</div>
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('Iniciales') }}</b></div>
-    <div class="col-sm-8">{!! $estudios->h_iniciales !!}</div>
-</div>
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('Fecha de Nacimiento') }}</b></div>
-    <div class="col-sm-8">{!! $estudios->h_dia !!} - {!! $estudios->h_mes !!} - {!! $estudios->h_anio !!}</div>
-</div>
-<div class="row">
-    <div class="col-sm-4"><b>{{ _i('País de Nacimiento') }}</b></div>
-    <div class="col-sm-8">@if($estudios->pais){{ $estudios->pais->name }}@endif</div>
-</div>
 
-@else
-    <div class="row">
-        <div class="col-sm-4"><b>{{ _i('Especie') }}</b></div>
-        <div class="col-sm-8">{!! $estudios->a_especie !!}</div>
+<div class="card">
+    <div class="card-body">
+
+        <div class="row">
+            <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Tipo de Estudio') }}</b></div>
+            <div class="col-sm-8">{!! $estudios->tipo !!}</div>
+        </div>
+
+        @if($estudios->tipo=='humano')
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Nombre') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->h_nombre !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Apellido') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->h_apellido !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Apodo') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->h_identifica !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Iniciales') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->h_iniciales !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Fecha de Nacimiento') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->h_dia !!} - {!! $estudios->h_mes !!} - {!! $estudios->h_anio !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('País de Nacimiento') }}</b></div>
+                <div class="col-sm-8">@if($estudios->pais){{ $estudios->pais->name }}@endif</div>
+            </div>
+
+        @else
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Especie') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->a_especie !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Nombre del Dueño') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->a_duenio !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Nombre del Animal') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->a_animal !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Iniciales') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->a_iniciales !!}</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"><b style="font-weight: 900">{{ _i('Fecha de Nacimiento') }}</b></div>
+                <div class="col-sm-8">{!! $estudios->a_dia !!} - {!! $estudios->a_mes !!} - {!! $estudios->a_anio !!}</div>
+            </div>
+        @endif
+
+        <hr>
+        <div class="row">
+            <div class="col-sm-12">
+                <a href="{{route('estudios.edit', $estudios->id)}}" class="btn btn-info btn-sm">{{ _i('Actualizar Datos') }}</a>
+            </div>
+        </div>
+
     </div>
-    <div class="row">
-        <div class="col-sm-4"><b>{{ _i('Nombre del Dueño') }}</b></div>
-        <div class="col-sm-8">{!! $estudios->a_duenio !!}</div>
-    </div>
-    <div class="row">
-        <div class="col-sm-4"><b>{{ _i('Nombre del Animal') }}</b></div>
-        <div class="col-sm-8">{!! $estudios->a_animal !!}</div>
-    </div>
-    <div class="row">
-        <div class="col-sm-4"><b>{{ _i('Iniciales') }}</b></div>
-        <div class="col-sm-8">{!! $estudios->a_iniciales !!}</div>
-    </div>
-    <div class="row">
-        <div class="col-sm-4"><b>{{ _i('Fecha de Nacimiento') }}</b></div>
-        <div class="col-sm-8">{!! $estudios->a_dia !!} - {!! $estudios->a_mes !!} - {!! $estudios->a_anio !!}</div>
-    </div>
-@endif
+</div>
