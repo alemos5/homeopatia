75f6e4c363d7b7e94ff09e0ddadded53845ce970
Manuel Fernandez <mfernandezphp@gmail.com>
2020-01-31 16:51:11 -0400

    revisando

--- resources/views/estudios/show_remedio.blade.php
+++ resources/views/estudios/show_remedio.blade.php
@@ -1,26 +1,27 @@
 <h2>{{ _i('Análisis desplegado') }}</h2>
 <hr>
-<table style="width: 100%; font-size: 14px;" id="data-table" class="table table-striped table-bordered dt-responsive nowrap">
-    <thead>
-    <tr>
-        <th>{{ _i('ID') }}</th>
-        <th>{{ _i('Reino') }}</th>
-        <th>{{ _i('Medicamento') }}</th>
-        <th>{{ _i('RSM') }}</th>
-        <th>{{ _i('Consonantes') }}</th>
-        <th>{{ _i('Clave') }}</th>
-        <th>{{ _i('Secuencia') }}</th>
-        <th>{{ _i('Recetado') }}</th>
-    </tr>
-    </thead> <?php  ?>
-    <tbody>
-    @foreach($remedios as $remedio)
+<div class="table-responsive">
+    <table style="width: 100%; font-size: 14px;" id="data-table" class="table table-striped table-bordered dt-responsive nowrap">
+        <thead>
         <tr>
-            <td>
-                {{$remedio->id}}
-            </td>
-            <td>
-                
+            <th>{{ _i('ID') }}</th>
+            <th>{{ _i('Reino') }}</th>
+            <th>{{ _i('Medicamento') }}</th>
+            <th>{{ _i('RSM') }}</th>
+            <th>{{ _i('Consonantes') }}</th>
+            <th>{{ _i('Clave') }}</th>
+            <th>{{ _i('Secuencia') }}</th>
+            <th>{{ _i('Recetado') }}</th>
+        </tr>
+        </thead> <?php  ?>
+        <tbody>
+        @foreach($remedios as $remedio)
+            <tr>
+                <td>
+                    {{$remedio->id}}
+                </td>
+                <td>
+
                     <?php
                     $dataReino = \App\Http\Controllers\EstudiosController::getImgReino($remedio->pregnancia);
                     //                    var_dump($dataReino);
@@ -34,59 +35,60 @@
                         </div>
                     </div>
 
-                
-            </td>
-            <td>
-                {{$remedio->nombre}}
-            </td>
-            <td>
-                <?php
-                echo \App\Http\Controllers\EstudiosController::getRsm(
-                    $remedio->id,
-                    $data['iniciales'],
-                    $data['dia'],
-                    $data['mes'],
-                    $data['anio'],
-                    $data['nombre'],
-                    $data['apellido']
-                );
-                ?>
-            </td>
 
-            <td>
-                
+                </td>
+                <td>
+                    {{$remedio->nombre}}
+                </td>
+                <td>
+                    <?php
+                    echo \App\Http\Controllers\EstudiosController::getRsm(
+                        $remedio->id,
+                        $data['iniciales'],
+                        $data['dia'],
+                        $data['mes'],
+                        $data['anio'],
+                        $data['nombre'],
+                        $data['apellido']
+                    );
+                    ?>
+                </td>
+
+                <td>
+
                     <?php
                     if ($remedio->puros == 1) {
                     ?><i class="fas fa-star"></i><?php
                     }
                     ?>
-                
-            </td>
-            <td>
-                
+
+                </td>
+                <td>
+
                     <?php
                     if ($remedio->tipoRemedioClave == 1) {
                     ?><i class="fas fa-star"></i><?php
                     }
                     ?>
-                
-            </td>
-            <td>
-                
+
+                </td>
+                <td>
+
                     <?php
                     $secuencia = \App\Http\Controllers\EstudiosController::getSecuencia($remedio->id, $data['apodo']);
                     if ($secuencia) {
                     ?><i class="fas fa-star"></i><?php
                     }
                     ?>
-                
-            </td>
-            <td>
-                
+
+                </td>
+                <td>
+
                     <input type="checkbox" value="{{$remedio->id}}" name="check_{{$remedio->id}}" id="check_{{$remedio->id}}" class="js-switch"/>
-                
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
